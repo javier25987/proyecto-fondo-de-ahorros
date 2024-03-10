a1f54d9f92745b715625d6de7778f871df588c2d
javier25987 <fernando71153049@gmail.com>
2024-03-10 18:07:07 +0300

    SoloPuseLaVersion

--- proyecto fondo.py
+++ proyecto fondo.py
@@ -28,4 +28,8 @@ class primera_ventana(tk.Tk, Funciones):
         self.button_crear_socio = tk.Button(self, text='crear socio', command=self.funcion_1)
         self.button_crear_socio.pack()
 
+        self.label_version = tk.Label(self, text='version 0,00,01 (ventanas)')
+        self.label_version.place(relx=0,rely=0.977)
+
+
 primera_ventana().mainloop()
