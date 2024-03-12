cd0be4921ce2faf955681c1cc8d364aceffbcf73
javier25987 <fernando71153049@gmail.com>
2024-03-13 02:27:54 +0300

    AjustePantalla

--- proyecto fondo.py
+++ proyecto fondo.py
@@ -15,21 +15,17 @@ class primera_ventana(tk.Tk, Funciones):
     def __init__(self):
         super().__init__()
 
-        ancho_pantalla = self.winfo_screenwidth()
-        alto_pantalla = self.winfo_screenheight()
+        self.state("zoomed")
 
         self.title(nombre_negocio)
 
-        self.geometry(f"{ancho_pantalla}x{alto_pantalla-80}+0+0")
-
-        self.label = tk.Label(self, text=f'{str(n)}')
+        self.label = tk.Label(self, text=n)
         self.label.pack()
 
         self.button_crear_socio = tk.Button(self, text='crear socio', command=self.funcion_1)
         self.button_crear_socio.pack()
 
-        self.label_version = tk.Label(self, text='version 0,00,01 (ventanas)')
-        self.label_version.place(relx=0,rely=0.977)
-
+        self.label_version = tk.Label(self, text='version 0,0,0')
+        self.label_version.place(relx=0, rely=0.975)
 
 primera_ventana().mainloop()
